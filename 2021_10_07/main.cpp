2cb955bae8d9ed1cfcf1ed95e0cf1f297d1b315e
SCsilla <91518832+SCsilla@users.noreply.github.com>
2021-10-07 07:38:50 +0300

    B Sz2

---
+++ 2021_10_07/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    int j, i, m[100][100], n;
+    cout<< "n=";
+    cin>> n;
+
+
+
+    for(i=0;i<n;i++){
+    for(j=0;j<n;j++){
+        m[i][j]=rand()%100;
+
+    }}
+
+    for(i=0;i<n;i++){
+    for(j=0;j<n;j++){
+            if(i>j){
+            m[i][j]=-m[i][j];
+
+    }
+            if(i<j){
+            m[i][j]=0;
+            }
+    }
+    }
+     cout<<endl;
+    for(i=0;i<n;i++){
+    for(j=0;j<n;j++){
+        cout<<m[i][j]<<" ";
+    }
+    cout<<endl;}
+    return 0;
+}
